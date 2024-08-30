41e0da88eb631b92e581288efe55c2d55e2eb874
gammar93 <gammargaryagdyyev@gmail.com>
2024-08-31 00:25:10 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 2.3"
+      version = "~> 4.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.3"  # Update to the latest version that supports darwin_arm64
     }
   }
 }
